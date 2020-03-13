c0e6b1540ed8eceaf90b86122bc6df9f878c2482
RNatvik <r_bn-95@hotmail.com>
2020-03-13 11:47:45 +0100

    Added indication for waterline in output plots. Restructured data storage

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import matplotlib.pyplot as plt
 from construction import Component, calculate_bm
 
 
-def save_data(bm, cg, cb, data, title='', scrap_run=True):
+def save_data(data, title='', scrap_run=True):
     if scrap_run:
         title = 'scrap'
     os.makedirs(os.path.dirname(f'graphs/{title}/data.txt'), exist_ok=True)
     with open(f'graphs/{title}/data.txt', 'w') as file:
-        file.write(data)
+        file.write(f'{data["tree"]}\n\n{data["text"]}')
+
+    cb = data['B']
+    cg = data['G']
+    bm = data['BM']
+    bottom_dist = data['bottom_dist']
+    submersion = data['submersion']
+    waterline = [d + submersion for d in bottom_dist]
     x = 0
     y = 1
     z = 2
     yx = [(cb[y] + bm[y], cb[x] + bm[x]), (cg[y], cg[x]), (cb[y], cb[x])]
-    xz = [(cb[x] + bm[x], cb[z] + bm[z]), (cg[x], cg[z]), (cb[x], cb[z])]
-    yz = [(cb[y] + bm[y], cb[z] + bm[z]), (cg[y], cg[z]), (cb[y], cb[z])]
+    xz = [(cb[x] + bm[x], cb[z] + bm[z]), (cg[x], cg[z]), (cb[x], cb[z]), (0, waterline[z])]
+    yz = [(cb[y] + bm[y], cb[z] + bm[z]), (cg[y], cg[z]), (cb[y], cb[z]), (0, waterline[z])]
     figYX = plot(yx, title=title, spice='Top view', axis_label=['Y', 'X'])
     figXZ = plot(xz, title=title, spice='Side view', axis_label=['X', 'Z'])
     figYZ = plot(yz, title=title, spice='Front view', axis_label=['Y', 'Z'])
@@ -40,7 +47,7 @@ def plot(xy, title='', spice='', axis_label=None):
     plt.plot(0, 0, 'o')
     for x, y in xy:
         plt.plot(x, y, 'x')
-    plt.legend(['origin', 'M', 'G', 'B'])
+    plt.legend(['origin', 'M', 'G', 'B', 'waterline'])
     plt.grid()
     plt.title(f'{title} {spice}')
     plt.xlabel(axis_label[0])
@@ -62,6 +69,7 @@ def main():
     ballast_density = 1025.0  # kg/m³
     additional_ballast_weight = 25
 
+    bottom_dist = [0, 0, -vertical_pipe_length]
     root = Component(2.92 + 0.26 + 0.939 + 0.162, 0, description='Platform')
     electronics = Component(
         7.18 + 0.046 + 0.007 + 0.1 + 0.302 + 0.004 + 0.19, volume=0,
@@ -152,31 +160,44 @@ def main():
     additional_ballast = Component(
         additional_ballast_weight, 0,
         description='additional ballast',
-        parent=root, parent_vector=[0, 0, -(vertical_pipe_length-0.15)]
+        parent=root, parent_vector=[0, 0, -(vertical_pipe_length - 0.15)]
     )
 
     cg, weight = root.calculate_cg()
     cb, volume, submersion, submersion_rate = root.calculate_cb(vertical_pipe_length, fluid_density=1025.0)
     bm = calculate_bm(pipe_radius, platform_width / 2, volume, num_poles=6)
-    data = f'Parameters:\n' \
-           f'    Platform width:         {platform_width} [m]\n' \
-           f'    Platform length:        {platform_length} [m]\n' \
-           f'    Pipe diameter:          {pipe_diameter} [m]\n' \
-           f'    Vertical pipe length:   {vertical_pipe_length} [m]\n' \
-           f'    Horizontal pipe length: {horizontal_pipe_length} [m]\n' \
-           f'    Ballast density:        {ballast_density} [kg/m³]\n' \
-           f'    Additional ballast:     {additional_ballast_weight} [kg]\n' \
-           f'Centre of gravity:   {[round(x, 5) for x in cg.tolist()]} (x, y, z)\n' \
-           f'Centre of buoyancy:  {[round(x, 5) for x in cb.tolist()]} (x, y, z)\n' \
-           f'Total weight:        {weight} [kg]\n' \
-           f'Submerged volume:    {volume} [m³]\n' \
-           f'Submersion / rate:   {submersion} [m] / {submersion_rate} [ratio]'
+    text = f'Parameters:\n' \
+        f'    Platform width:         {platform_width} [m]\n' \
+        f'    Platform length:        {platform_length} [m]\n' \
+        f'    Pipe diameter:          {pipe_diameter} [m]\n' \
+        f'    Vertical pipe length:   {vertical_pipe_length} [m]\n' \
+        f'    Horizontal pipe length: {horizontal_pipe_length} [m]\n' \
+        f'    Ballast density:        {ballast_density} [kg/m³]\n' \
+        f'    Additional ballast:     {additional_ballast_weight} [kg]\n' \
+        f'Centre of gravity:   {[round(x, 5) for x in cg.tolist()]} (x, y, z)\n' \
+        f'Centre of buoyancy:  {[round(x, 5) for x in cb.tolist()]} (x, y, z)\n' \
+        f'Total weight:        {weight} [kg]\n' \
+        f'Submerged volume:    {volume} [m³]\n' \
+        f'Submersion / rate:   {submersion} [m] / {submersion_rate} [ratio]'
+    data = {
+        'G': cg,
+        'B': cb,
+        'BM': bm,
+        'weight': weight,
+        'volume': volume,
+        'submersion': submersion,
+        'submersion_rate': submersion_rate,
+        'bottom_dist': bottom_dist,
+        'tree': root.get_tree(),
+        'text': text
+    }
+
     save_data(
-        bm, cg, cb, data,
+        data,
         title=f'110mm_6p_070x100_{additional_ballast_weight}_additional_ballast_height_compensated',
         scrap_run=True
     )
-    print(data)
+    print(f'{data["tree"]}\n\n{data["text"]}')
 
 
 if __name__ == '__main__':
